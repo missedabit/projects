2bf722a173d6dce96c5ab1067624e1cb928bb5d4
missedabit <nadeem@missedabit.com>
2014-08-30 21:27:54 +0100

    add js

--- js/stuff.js
+++ js/stuff.js
@@ -4,4 +4,23 @@ $(document).ready( function() {
 		$(this).css("color","green");
 });
 
+	$("#block1").css("float","left").css("width","200px").css("height","200px").css("background-color","red").text("this is a red");
+	$("#block2").css("float","left").css("width","200px").css("height","200px").css("background-color","yellow").text("this is a yellow");
+
+	$("#block1").click( function() {
+
+		$(this).slideToggle('slow');
+
+	} );
+
+	$("button").click( function(){
+
+		var inputItem = $("input").val();
+
+		$("#addStuff").html(inputItem);
+
+
+
+	 });
+
 })
