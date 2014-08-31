ae7949e060083c8c7e1c7b76879d994c024e875d
missedabit <nadeem@missedabit.com>
2014-08-31 07:42:45 +0100

    add files

--- js/stuff.js
+++ js/stuff.js
@@ -1,26 +1,26 @@
-$(document).ready( function() {
+$(document).ready( function() { 
 
 	$("#banana").on("click",function(){
 		$(this).css("color","green");
-});
+	});
 
 	$("#block1").css("float","left").css("width","200px").css("height","200px").css("background-color","red").text("this is a red");
 	$("#block2").css("float","left").css("width","200px").css("height","200px").css("background-color","yellow").text("this is a yellow");
 
 	$("#block1").click( function() {
-
 		$(this).slideToggle('slow');
-
 	} );
 
 	$("button").click( function(){
-
 		var inputItem = $("input").val();
-
 		$("#addStuff").html(inputItem);
-
-
-
 	 });
+
+	$(".loader").click( function(){
+		$("#loadContent").load("load.htm", function(data){
+			var title = "Title:";
+			$(this).html("<p>"+title+data+"</p>")
+		});
+	});
 
 })
